918da06068d1861aa50ecead4e91da0b08ceead6
Kevin Johnson <kcavemanj@gmail.com>
2013-10-12 12:05:16 -0400

    Adds admin dashboard elements

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,5 +1,13 @@
 <?php include_once('views/global/header.php'); ?>
 
-This is probably where the admin dashboard would go.
+<ul class="nav nav-list">
+	<li><a href="<?php echo SITE_ROOT; ?>admin/catalog">Manage Catalog</a></li>
+	<li><a href="<?php echo SITE_ROOT; ?>admin/orders">Place Orders</a></li>
+	<li><a href="<?php echo SITE_ROOT; ?>admin/reports">Generate Reports</a></li>
+	<li><a href="<?php echo SITE_ROOT; ?>admin/profile">Update Admin Profile</a></li>
+	<li><a href="<?php echo SITE_ROOT; ?>admin/logout">Logout Admim</a></li>
+</ul>
+
+<div>
 
 <?php include_once('views/global/footer.php'); ?>
