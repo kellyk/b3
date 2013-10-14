d0bad7f9e28ccb24347f090b965d643d17a85e44
Kevin Johnson <kcavemanj@gmail.com>
2013-10-14 17:11:09 -0400

    initial setup for profile edit mockup

--- views/admin/profile.php
+++ views/admin/profile.php
@@ -1,1 +1,24 @@
-Insert profile here.
+<div>
+	<form role="form">
+		<label for="username">Username:</label>
+		<input type="text" id="username" name="username" value="kevinj" disabled />
+		<label for="newPin">New PIN:</label>
+		<input type="password" id="newPin" name="newPin" value="*****" />
+		<label for="retypePin">Retype New Pin:</label>
+		<input type="password" id="retypePin" name="retypePin" value="*****" />
+		<label for="fName">First Name: </label>
+		<input type="text" name="fName" id="fName" value="Kevin" />
+		<label for="lName">Last Name: </label>
+		<input type="text" name="lName" id="lName" value="Johnson" />
+		<label for="address">Address:</label>
+		<input type="text" name="address" id="address" value="123 Pine Dr." />
+		<label for="city">City: </label>
+		<input type="text" name="city" id="city" value="Ypsilanti" />
+		<label for="state">State: </label>
+		<select name="state" id="state">
+			<option selected>MI</option>
+		</select>
+		
+	</form>
+</div>
+</div>
