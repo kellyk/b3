8a4a3472fa8415c40b4c4220ba05f2f2e4b7e39a
Kevin Johnson <kcavemanj@gmail.com>
2013-10-14 17:40:58 -0400

    Fix profile update page.

--- views/admin/profile.php
+++ views/admin/profile.php
@@ -1,24 +1,24 @@
 <div>
 	<form role="form">
+		<div class="form-group">
 		<label for="username">Username:</label>
-		<input type="text" id="username" name="username" value="kevinj" disabled />
+		<input class="form-control" type="text" id="username" name="username" value="kevinj" disabled />
 		<label for="newPin">New PIN:</label>
-		<input type="password" id="newPin" name="newPin" value="*****" />
+		<input class="form-control" type="password" id="newPin" name="newPin" value="*****" />
 		<label for="retypePin">Retype New Pin:</label>
-		<input type="password" id="retypePin" name="retypePin" value="*****" />
+		<input class="form-control" type="password" id="retypePin" name="retypePin" value="*****" />
 		<label for="fName">First Name: </label>
-		<input type="text" name="fName" id="fName" value="Kevin" />
+		<input class="form-control" type="text" name="fName" id="fName" value="Kevin" />
 		<label for="lName">Last Name: </label>
-		<input type="text" name="lName" id="lName" value="Johnson" />
+		<input class="form-control" type="text" name="lName" id="lName" value="Johnson" />
 		<label for="address">Address:</label>
-		<input type="text" name="address" id="address" value="123 Pine Dr." />
+		<input class="form-control" type="text" name="address" id="address" value="123 Pine Dr." />
 		<label for="city">City: </label>
-		<input type="text" name="city" id="city" value="Ypsilanti" />
+		<input class="form-control" type="text" name="city" id="city" value="Ypsilanti" />
 		<label for="state">State: </label>
-		<select name="state" id="state">
+		<select class="form-control" name="state" id="state">
 			<option selected>MI</option>
 		</select>
-		
+		</div>
 	</form>
-</div>
 </div>
