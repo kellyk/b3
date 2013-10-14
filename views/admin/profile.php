06fa395e9c0945a9d8bf9bd07de198272ffa2ad4
Kevin Johnson <kcavemanj@gmail.com>
2013-10-14 17:57:08 -0400

    Finish mock up of profile update page.

--- views/admin/profile.php
+++ views/admin/profile.php
@@ -1,24 +1,28 @@
 <div>
 	<form role="form">
 		<div class="form-group">
-		<label for="username">Username:</label>
-		<input class="form-control" type="text" id="username" name="username" value="kevinj" disabled />
-		<label for="newPin">New PIN:</label>
-		<input class="form-control" type="password" id="newPin" name="newPin" value="*****" />
-		<label for="retypePin">Retype New Pin:</label>
-		<input class="form-control" type="password" id="retypePin" name="retypePin" value="*****" />
-		<label for="fName">First Name: </label>
-		<input class="form-control" type="text" name="fName" id="fName" value="Kevin" />
-		<label for="lName">Last Name: </label>
-		<input class="form-control" type="text" name="lName" id="lName" value="Johnson" />
-		<label for="address">Address:</label>
-		<input class="form-control" type="text" name="address" id="address" value="123 Pine Dr." />
-		<label for="city">City: </label>
-		<input class="form-control" type="text" name="city" id="city" value="Ypsilanti" />
-		<label for="state">State: </label>
-		<select class="form-control" name="state" id="state">
-			<option selected>MI</option>
-		</select>
+			<label for="username">Username:</label>
+			<input class="form-control" type="text" id="username" name="username" value="kevinj" disabled />
+			<label for="newPin">New PIN:</label>
+			<input class="form-control" type="password" id="newPin" name="newPin" value="*****" />
+			<label for="retypePin">Retype New Pin:</label>
+			<input class="form-control" type="password" id="retypePin" name="retypePin" value="*****" />
+			<label for="fName">First Name: </label>
+			<input class="form-control" type="text" name="fName" id="fName" value="Kevin" />
+			<label for="lName">Last Name: </label>
+			<input class="form-control" type="text" name="lName" id="lName" value="Johnson" />
+			<label for="address">Address:</label>
+			<input class="form-control" type="text" name="address" id="address" value="123 Pine Dr." />
+			<label for="city">City: </label>
+			<input class="form-control" type="text" name="city" id="city" value="Ypsilanti" />
+			<label for="state">State: </label>
+			<select class="form-control" name="state" id="state">
+				<option selected>MI</option>
+			</select>
+			<label for="zip">Zip Code:</label>
+			<input class="form-control" type="text" name="zip" id="zip" value="48197" />
+			<label for"hireDate">Hire Date: </label>
+			<input class="form-control" type="text" name="hireDate" id="hireDate" value="2010-11-29" disabled />
 		</div>
 	</form>
 </div>
