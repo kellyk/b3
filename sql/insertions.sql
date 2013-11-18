c5c2c9e6a8d8e0a349c0afec682cea7d95e42697
Ringo Stahl <ringo.stahl@gmail.com>
2013-11-18 11:46:58 -0500

    Update insertions.sql

--- sql/insertions.sql
+++ sql/insertions.sql
@@ -21,3 +21,22 @@ INSERT INTO wrote VALUES (0077274334, 5);
 INSERT INTO review VALUES (DEFAULT, 0073523321, 'Great book. Helped me learn SQL');
 INSERT INTO review VALUES (DEFAULT, 0810994429, 'I hate art now. Worst book ever');
 INSERT INTO review VALUES (DEFAULT, 0077274334, 'Not a bad primer on biology. A little expensive.');
+
+/* user */
+INSERT INTO user VALUES('Jonny97', 1234, 'John', 'Doe', 'Chicago', '56 Main St', 60616, 'IL', 'MasterCard', 1234567890123456);
+INSERT INTO user VALUES('Mary', 4321, 'Mary', 'Smith', 'San Francisco', '89 7th Ave', 94101, 'CA', 'Discover', 6734568901234512);
+INSERT INTO user VALUES('Tom', 9021, 'Tom', 'Kulic', 'Johnson City', '142 Montgomery Rd', 37601, 'TN', 'MasterCard', 8901231245634567);
+INSERT INTO user VALUES('Sue', 9021, 'Susanne', 'Lovelace', 'Ann Arbor', '96 Packard St', 48104, 'MI', NULL, NULL);
+INSERT INTO user VALUES('Bill', 9021, 'William', 'Carmichael', 'Ypsilanti', '134 Carpenter Rd', 48197, 'MI', NULL, NULL);
+INSERT INTO user VALUES('Ahmed', 9021, 'Ahmed', 'Hassan', 'Ypsilanti', '73 Clubview Rd', 48197, 'MI', NULL, NULL);
+
+/* administrator */
+INSERT INTO administrator VALUES('Sue', '2012-07-21');
+INSERT INTO administrator VALUES('Bill', '2012-01-01');
+INSERT INTO administrator VALUES('Ahmed', '2013-03-12');
+
+/* phone_number */
+INSERT INTO administrator VALUES('Sue', '1238742937');
+INSERT INTO administrator VALUES('Sue', '4238793127');
+INSERT INTO administrator VALUES('Bill', '9338747122');
+INSERT INTO administrator VALUES('Ahmed', '3874122929');
