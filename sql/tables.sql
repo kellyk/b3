2a8060f0f871ae9448fa6b7a87c9aa5f6695cbf2
Kevin Johnson <kcavemanj@gmail.com>
2013-11-18 19:32:53 -0500

    Table formatting and references.

--- sql/tables.sql
+++ sql/tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS user (
 	city		VARCHAR(32) NOT NULL,
 	address		VARCHAR(32) NOT NULL,
 	zip		NUMERIC(5,0) NOT NULL, 
-	state		VARCHAR(32) NOT NULL, /* maybe we could use this http://kimbriggs.com/computers/computer-notes/mysql-notes/mysql-create-state-table.file*/
+	state		CHARACTER(2) NOT NULL, /* maybe we could use this http://kimbriggs.com/computers/computer-notes/mysql-notes/mysql-create-state-table.file*/
 	cctype		ENUM('MasterCard','VISA','American Express', 'Discover'), # more?
 	ccnumber	NUMERIC(16), 
 	PRIMARY KEY (username)
@@ -17,39 +17,39 @@ CREATE TABLE IF NOT EXISTS user (
 )ENGINE='InnoDB';
 
 CREATE TABLE IF NOT EXISTS book(
-	isbn INTEGER NOT NULL,
-	title VARCHAR(255),
-	category VARCHAR(50),
-	publisher VARCHAR(255),
-	deleted BOOLEAN,
-	inventory_quantity SMALLINT,
-	inventory_minimum	 SMALLINT,
-	year_published SMALLINT,
-	price DECIMAL(6,2),
+	isbn 			INTEGER NOT NULL,
+	title 			VARCHAR(255),
+	category		VARCHAR(50),
+	publisher 		VARCHAR(255),
+	deleted 		BOOLEAN,
+	inventory_quantity 	SMALLINT,
+	inventory_minimum	SMALLINT,
+	year_published 		SMALLINT,
+	price 			DECIMAL(6,2),
 	PRIMARY KEY (isbn)
 ) ENGINE='InnoDB';
 
 CREATE TABLE IF NOT EXISTS author(
-	id INTEGER NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
+	id 		INTEGER NOT NULL AUTO_INCREMENT,
+	first_name 	VARCHAR(255),
+	last_name 	VARCHAR(255),
 	PRIMARY KEY (id)
 ) ENGINE='InnoDB';
 
 CREATE TABLE IF NOT EXISTS wrote(
-	isbn INTEGER NOT NULL,
-	author_id INTEGER NOT NULL,
+	isbn 		INTEGER NOT NULL,
+	author_id 	INTEGER NOT NULL,
 	PRIMARY KEY (isbn, author_id),
-	FOREIGN KEY (isbn) REFERENCES book,
+	FOREIGN KEY (isbn) REFERENCES book (isbn),
 	FOREIGN KEY (author_id) REFERENCES author(id)
 ) ENGINE='InnoDB';
 
 CREATE TABLE IF NOT EXISTS review (
-	review_number INTEGER NOT NULL AUTO_INCREMENT,
-	isbn INTEGER NOT NULL,
-	review_text BLOB,
+	review_number 		INTEGER NOT NULL AUTO_INCREMENT,
+	isbn 			INTEGER NOT NULL,
+	review_text 		TEXT,
 	PRIMARY KEY (review_number, isbn),
-	FOREIGN KEY (isbn) REFERENCES book
+	FOREIGN KEY (isbn) REFERENCES book (isbn)
 ) ENGINE='InnoDB';
 
 CREATE TABLE IF NOT EXISTS orders (
@@ -79,7 +79,7 @@ CREATE TABLE IF NOT EXISTS cart (
 
 CREATE TABLE IF NOT EXISTS cart_item (
 	username	VARCHAR(32) NOT NULL REFERENCES cart (username) ON DELETE CASCADE,
-	isbn		varchar(11) NOT NULL REFERENCES book (isbn) ON DELETE CASCADE,
+	isbn		INTEGER NOT NULL REFERENCES book (isbn) ON DELETE CASCADE,
 	quantity	INTEGER NOT NULL DEFAULT 1,
 	PRIMARY KEY (username, isbn)
 ) ENGINE='InnoDB';
