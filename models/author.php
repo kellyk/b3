637274fa8655281e2e69c1711b80285c57ecc1b8
Kevin Johnson <kcavemanj@gmail.com>
2013-12-08 18:02:59 -0500

    Add authro model

---
+++ models/author.php
@@ -0,0 +1,46 @@
+<?php
+require_once('BaseModel.php');
+
+class AuthorModel extends BaseModel {
+
+	public function __construct() {
+		parent::__construct();
+		
+		$this->author_def = array(
+			"id"         => "INTEGER",
+			"first_name" => "STRING",
+			"last_name"  => "STRING",
+		);
+	}
+
+	public function getByID($id) {
+		return $this->performQuery(
+			"SELECT *
+			FROM Author
+			WHERE id = $id;"
+		);
+	}	
+
+	public function getByISBN($isbn) {
+		return $this->performQuery(
+			"SELECT *
+			FROM author, wrote
+			WHERE author.id = wrote.author_id
+			AND wrote.isbn = '$isbn';"
+		);
+	}
+
+	public function getByName($first, $last) {
+		return $this->performQuery(
+			"SELECT *
+			FROM author
+			WHERE first_name = '$first'
+			AND last_name = '$last';"
+		);
+	}
+
+	public function create($data) {
+		return $this->insert($data, "author");
+	}
+};
+
