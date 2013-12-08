96cd0531204f69f4032e7df391a68f180d7f2b2e
Kevin Johnson <kcavemanj@gmail.com>
2013-12-08 16:59:21 -0500

    Finish off and document the insert helper method.

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -47,8 +47,56 @@ class BaseModel {
 		return $result;
 	}
 
+
+	/* A function which simplifies inserts.
+	Params: 
+		$data - An associative array where the keys correspond to column names and values similarly.
+		$table - the name of the table on which to insert.
+
+	usage:
+		This fuction assumes all data are strings unless there is a table definition
+		in the model indicating which values are integers. Definitions must be stored
+		in a model with the name $table_def . A definition might be declared in a 
+		constructor like the following:
+
+		public function __construct() {
+			parent::__construct();
+			
+			$this->book_def = array(
+				"isbn"               => "STRING",
+				"title"              => "STRING",
+				"category"           => "STRING",
+				"publisher"          => "STRING",
+				"deleted"            => "INTEGER",
+				"inventory_quantity" => "INTEGER",
+				"inventory_minimum"  => "INTEGER",
+				"year_published"     => "INTEGER",
+				"price"              => "DECIMAL",
+			);
+		}
+	
+		A call like below will produce an appropriate query and run it.
+
+		//Create book. Parameter is an associative array.
+		function createBook($detail) {
+			return $this->insert(array(
+				"isbn"               => '003241314',
+				"title"              => "Fear and Loathing in Las Vegas",
+				"price"              => 13.50,
+				"inventory_quantity" => 10,
+			), "book");
+		}
+
+
+		This will run the following query:
+		
+		INSERT INTO book (isbn, title, price, inventory_quantity) 
+			VALUES('003241314', 'Fear and Loathing in Las Vegas', 13.5, 10);		
+	*/
+	
 	public function insert($data, $table) {
 		$def = $table . "_def";
+		$def = $this->$def;
 		$keys = array_keys($data);
 		$sql = "INSERT INTO $table (";
 		foreach ($keys as $key) {
@@ -56,9 +104,9 @@ class BaseModel {
 		}
 
 		// replace trialing comma with parenth.
-		preg_replace('/\, $/', ") VALUES(", $sql);
+		$sql = preg_replace('/\, $/', ") VALUES(", $sql);
 		foreach ($keys as $key) {
-			if ($this->$def[$key] == 'INTEGER' || $this->$def[$key] == 'DECIMAL') {
+			if ($def[$key] == 'INTEGER' || $def[$key] == 'DECIMAL') {
 				$sql = $sql . $data[$key] . ", ";
 			}
 			else {
@@ -66,7 +114,8 @@ class BaseModel {
 			}
 		}
 
-		preg_replace('/\, $/', ");", $sql);
-		die($sql);
+		$sql = preg_replace('/\, $/', ");", $sql);
+		
+		return $this->performWrite($sql);
 	}
 }
