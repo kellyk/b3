b1e6874556d23403aae077ee9ff138bc9be443a0
Kelly King <kelly.danielle.king@gmail.com>
2013-12-08 17:06:57 -0500

    update markup for register page

--- views/register.php
+++ views/register.php
@@ -3,31 +3,31 @@
 	<h3>Register!</h3>
 		<div class="row">
 
-		<form role="form">
+		<form role="form" method="POST" action="<?php echo SITE_ROOT ?>profile/register">
 			<div class="col-lg-5">
 			  	<div class="form-group">
-			    	<input type="text" class="form-control" id="username" placeholder="Username">
+			    	<input type="text" name="username" class="form-control" placeholder="Username" required />
 			  	</div>
 			  	<div class="form-group">
-				   <input type="text" class="form-control" id="pin" placeholder="Pin (4-6 digits)" required />
+				   <input type="text" name="pin" class="form-control" placeholder="Pin (4-6 digits)" required />
 				</div>
 				<div class="form-group">
-				    <input type="text" class="form-control" id="verifyPin" placeholder="Verify Pin" required />
+				    <input type="text" name="repin" class="form-control" placeholder="Verify Pin" required />
 				</div>
 				<div class="form-group">
-				    <input type="text" class="form-control" id="fName" placeholder="First Name">
+				    <input type="text" name="firstname" class="form-control" placeholder="First Name" required />
 				</div>
 				<div class="form-group">
-				    <input type="text" class="form-control" id="lName" placeholder="Last Name">
+				    <input type="text" name="lastname" class="form-control" placeholder="Last Name" required />
 				</div>
 				<div class="form-group">
-				    <input type="text" class="form-control" id="address" placeholder="Street">
+				    <input type="text" name="street" class="form-control" placeholder="Street" required />
 				</div>
 				<div class="form-group">
-				    <input type="text" class="form-control" id="city" placeholder="City">
+				    <input type="text" name="city" class="form-control" placeholder="City" required />
 				</div>
 				<div class="form-group">
-				    <select type="text" class="form-control" id="state">
+				    <select type="text" name="state" class="form-control">
 						<option value="AL">Alabama</option>
 						<option value="AK">Alaska</option>
 						<option value="AZ">Arizona</option>
@@ -80,14 +80,14 @@
 						<option value="WI">Wisconsin</option>
 						<option value="WY">Wyoming</option>
 				    </select><br />
-				    <input type="text" class="form-control" id="zip" placeholder="Zip Code">
+				    <input type="text" name="zip" class="form-control" placeholder="Zip Code" required />
 				</div>
 			</div>
 			<div class="col-lg-1"></div>
 			<div class="col-lg-6">
 				<div class="form-group">
 				    <label for="cc-type">Credit Card</label>
-				    <select class="form-control" id="cc-type">
+				    <select name="card_type" class="form-control">
 				    	<option value="visa">Visa</option>
 				    	<option value="mastercard">Mastercard</option>
 				    	<option value="discover">Discover</option>
@@ -95,11 +95,11 @@
 				</div>
 			    <div class="form-group">
 				    <label for="cc-number">Number</label>
-				    <input type="text" class="form-control" id="cc-number" placeholder="ex. 1234 5678 9012 3456">
+				    <input type="text" name="card_number" class="form-control" id="cc-number" placeholder="ex. 1234 5678 9012 3456" required />
 				</div>
 				<div class="form-group">
 				    <label for="cc-month">Month</label>
-				    <select class="form-control" id="cc-month">
+				    <select class="form-control" name="card_month">
 				    	<option value="1">Jan</option>
 				    	<option value="2">Feb</option>
 				    	<option value="3">Mar</option>
@@ -107,8 +107,7 @@
 				</div>
 				<div class="form-group">
 				    <label for="cc-year">Year</label>
-				    <select class="form-control" id="cc-year">
-				    	<option value="2013">2013</option>
+				    <select class="form-control" name="card_year">
 				    	<option value="2014">2014</option>
 				    	<option value="2015">2015</option>
 				    	<option value="2016">2016</option>
@@ -117,7 +116,7 @@
 				    </select>
 				</div>
 				<div class="form-group">
-					<button type="submit" class="btn btn-lg btn-success">Sign up</button>
+					<input type="submit" value="Submit" class="btn btn-lg btn-success" />
 				</div>
 			</div>
 		</form>
