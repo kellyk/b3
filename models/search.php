aa3c76720d7cf9dfcf187ec7e8f6a8776fbdbdc0
Kevin Johnson <kcavemanj@gmail.com>
2013-12-09 01:55:09 -0500

    Don't show deleted books to the customer

--- models/search.php
+++ models/search.php
@@ -23,6 +23,11 @@ class SearchModel extends BaseModel {
 	            WHERE wrote.isbn = book.isbn 
 	            AND wrote.author_id = author.id";
 	    
+		// If the user is not an administrator,
+		// Dont' show deleted books.
+		if (!$_SESSION['administrator']) {
+			$query = $query . " AND ( book.deleted = 0 OR book.deleted IS NULL) ";
+		}
 	    // since $fields was a multiple select input, we need to iterate through
 	    // each selection and find any matches. Could be refactored to be more modular.
 	    $fieldStatements = array();
