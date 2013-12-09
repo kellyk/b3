6484878048b6c0956cc26d7383d9981860bda002
Kevin Johnson <kcavemanj@gmail.com>
2013-12-09 16:44:02 -0500

    Add support for updating admin users.

--- controllers/admin.php
+++ controllers/admin.php
@@ -267,6 +267,26 @@ class Admin extends BaseController {
 		$this->_admin_skin('views/admin/profile.php', $args);
 	}
 
+	public function profile_update() {
+
+		$userModel = new UserModel();
+		$user = $userModel->getUser($_POST['username']);
+		$user = $user[0];
+		
+		foreach (array_keys($userModel->user_def) as $col) {
+			if (isset($_POST[$col]) && $_POST[$col] != $user[$col]) {
+				if ($col == 'PIN' && $_POST[$col] != '*****') {
+					$userModel->update($user['username'], $col, $_POST[$col]);
+				}
+				elseif ($col != 'PIN') {
+					$userModel->update($user['username'], $col, $_POST[$col]);
+				}
+			}
+		}
+
+		header('Location: ' . SITE_ROOT . "admin");
+	}
+
 	public function logout() {
 		// log user out and redirect to homepage
 		session_destroy();
--- views/admin/profile.php
+++ views/admin/profile.php
@@ -1,14 +1,20 @@
 <?php 
 	$user = $args['user'];
+	$action = SITE_ROOT . "admin/profile_";
+	$action = $user ? $action . "update" : $action . "create";
 ?>
 <div>
-	<form role="form">
+	<form role="form" action="<?php echo $action; ?>" method="POST">
 		<div class="form-group">
 			<label for="username">Username:</label>
+			<?php if ($user) { ?>
 			<input class="form-control" type="text" id="username_disabled" value="<?php echo $user[username]; ?>" disabled />
 			<input type="hidden" id="username" name="username" value="<?php echo $user[username]; ?>" />
+			<?php } else { ?>
+			<input class="form-control" type="text" id="username" value="" />
+			<?php } ?>
 			<label for="newPin">New PIN:</label>
-			<input class="form-control" type="password" id="newPin" name="newPin" value="*****" />
+			<input class="form-control" type="password" id="newPin" name="PIN" value="*****" />
 			<label for="retypePin">Retype New Pin:</label>
 			<input class="form-control" type="password" id="retypePin" name="retypePin" value="*****" />
 			<label for="fName">First Name: </label>
@@ -24,8 +30,14 @@
 			<label for="zip">Zip Code:</label>
 			<input class="form-control" type="text" name="zip" id="zip" value="<?php echo $user['zip']; ?>" />
 			<label for"hireDate">Hire Date: </label>
+			<?php if ($user) { ?>
 			<input class="form-control" type="text" name="hire_date" id="hireDate" value="<?php echo $user['hire_date']; ?>" disabled />
 			<input type="hidden" name="hire_date" id="hire_date" value="<?php echo $user['hire_date']; ?>" />
+			<?php } else { ?>
+			<input class="form-control" type="text" name="hire_date" id="hireDate" value="" />
+			<?php } ?>
+			<br />
+			<input class="form-control" type="submit" value="<?php echo $user ? "Update" : "Create" ?>" />
 		</div>
 	</form>
 </div>
